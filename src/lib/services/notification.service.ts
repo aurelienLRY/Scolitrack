df57ec0f23e0065be44474cac194fc7b736d5fc0
aurelienLRY <83220559+aurelienLRY@users.noreply.github.com>
2025-03-26 21:42:51 +0100

    Correction du nom de l'application de "Scolitack" à "Scolitrack" dans la configuration de l'application. Ajout d'un nouveau service pour la gestion des notifications backend, incluant des fonctionnalités pour créer, envoyer et gérer les notifications des utilisateurs.

---
+++ src/lib/services/notification.service.ts
@@ -0,0 +1,19 @@
+/**
+ * Service pour la gestion des notifications backend
+ * Préparation pour l'envoi de notification à l'utilisateur
+ * Enregistrer la notification dans une table de la base de données
+ *
+ * */
+
+/* 
+    Créer une notification
+    Envoyer une notification
+    Voir les notifications 
+    Voir une notification
+    voir les notification d'un utilisateur
+    l'utilisateur marque la notification comme lue
+    l'utilisateur supprime la notification
+    l'utilisateur supprime toutes les notifications
+    l'utilisateur supprime toutes les notifications lues
+    l'utilisateur supprime toutes les notifications non lues
+*/
